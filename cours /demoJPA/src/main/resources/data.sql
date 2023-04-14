83fe6032a8a5eb77dc06badd3673ea4306818c61
Fatima Kaaouan <fatima.kaaouan@octo.com>
2023-04-14 22:57:08 +0200

    git commit -m 'demoJPA'

---
+++ cours /demoJPA/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO article (id, ref, designation, prixHT, tva) VALUES (1, '123', 'vélo', 124, 0.2);
+INSERT INTO article (id, ref, designation, prixHT, tva) VALUES (2, '698', 'TV', 60, 0.2);
+INSERT INTO article (id, ref, designation, prixHT, tva) VALUES (3, '555', 'radio', 20, 0.2);
+INSERT INTO auteur (id, nom, prenom) VALUES (5, 'victor', 'hugo');
+INSERT INTO article (id, ref, designation, prixHT, tva) VALUES (4, '254', 'germinal', 2, 0.2);
+INSERT INTO livre (id, titre, categorie, auteur) VALUES (4, 'germinal', 'roman', 5);
+
